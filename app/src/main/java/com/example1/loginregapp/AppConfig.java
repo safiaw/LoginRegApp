493a86bcdb539da79541c6df32ffd5feb3ca08a4
safiaw <wahdat.safia@gmail.com>
2018-03-09 21:14:34 +0530

    Initial commit

---
+++ app/src/main/java/com/example1/loginregapp/AppConfig.java
@@ -0,0 +1,13 @@
+package com.example1.loginregapp;
+
+/**
+ * Created by lenovo1 on 07-03-2018.
+ */
+
+public class AppConfig {
+    // Server user login url
+    public static String URL_LOGIN = "http://127.0.0.1/android_login_api/login.php";
+
+    // Server user register url
+    public static String URL_REGISTER = "http://127.0.0.1/android_login_api/register.php";
+}
---
+++ app/src/main/java/com/example1/loginregapp/MainActivity.java
@@ -0,0 +1,31 @@
+package com.example1.loginregapp;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        TextView loginScreen = (TextView) findViewById(R.id.loginTextview);
+        loginScreen.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(), LoginActivity.class);
+                startActivity(intent);
+            }
+        });
+        TextView registerScreen = (TextView) findViewById(R.id.registerTextview);
+        registerScreen.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(), RegistrationActivity.class);
+                startActivity(intent);
+            }
+        });
+    }
+}
